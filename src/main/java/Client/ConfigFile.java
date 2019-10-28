012862b046124336e83e70e75d8bfa574462acba
Tiago Oliveira <talm.oliveira@campus.fct.unl.pt>
2019-10-28 17:34:32 +0000

    corrected configFile

--- src/main/java/Client/ConfigFile.java
+++ src/main/java/Client/ConfigFile.java
@@ -6,69 +6,24 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
 public class ConfigFile {
+
+
+	public Node readFile( File ficheiro, String ip) throws ParserConfigurationException, SAXException, IOException {
+		File file = ficheiro;
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse(file);
+		NodeList tempdoc= document.getElementsByTagName(ip);
+
+		Node temp= tempdoc.item(0);
+		return temp;
 	
-private String sid;
-private String sea;
-private int seaks;
-private String mode;
-private String padding;
-private String hash;
-private String mac;
-private int makks;
-private File xml;
+	}
 
-public ConfigFile(File ficheiro) {
-	xml=ficheiro;
-}
-
-  public void readFile( File ficheiro) throws ParserConfigurationException, SAXException, IOException {
-	  File file = ficheiro;
-	  DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-	  DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-	  Document document = documentBuilder.parse(file);
-	  sid = document.getElementsByTagName("sid").item(0).getTextContent();
-	  sea = document.getElementsByTagName("sea").item(0).getTextContent();
-	  seaks = Integer.parseInt(document.getElementsByTagName("seaks").item(0).getTextContent());
-	  mode = document.getElementsByTagName("mode").item(0).getTextContent();
-	  padding = document.getElementsByTagName("padding").item(0).getTextContent();
-	  hash = document.getElementsByTagName("inthash").item(0).getTextContent();
-	  mac = document.getElementsByTagName("mac").item(0).getTextContent();
-	  makks = Integer.parseInt(document.getElementsByTagName("makks").item(0).getTextContent());
-  }
-  
-  public String getSid(){
-	  return sid;
-  }
-
-  public int getSeaks(){
-	  return seaks;
-  }
-  
-  public String getMode(){
-	  return mode;
-  }
-  
-  public String getSea(){
-	  return sea;
-  }
-  
-  public String getPadding(){
-	  return padding;
-  }
-  
-  public String getHash(){
-	  return hash;
-  }
-  
-  public String getMac(){
-	  return mac;
-  }
-  
-  public int getMakks(){
-	  return makks;
-  }
 }
