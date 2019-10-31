6b22c2bf6de8cfc632e6adc06ae01f1338a231e8
Tiago Oliveira <talm.oliveira@campus.fct.unl.pt>
2019-10-31 00:26:56 +0000

    added test that was accidentaly removed

--- src/main/java/Utils/EndpointReader.java
+++ src/main/java/Utils/EndpointReader.java
@@ -1,6 +1,29 @@
 package Utils;
 
 import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import SMCP.EndpointConfiguration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class EndpointReader {
+
+    public static EndpointConfiguration readFile(
+        File ficheiro,
+        String ip
+    ) throws ParserConfigurationException, SAXException, IOException {
+        EndpointConfiguration config;
+
+       package Utils;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -63,5 +86,27 @@ public class EndpointReader {
 
         config = new EndpointConfiguration(ip, sid, sea, seaks, mode, padding, hash, mac, makks);
         return config;
+    }
+}
+
+    }
+
+    public static EndpointConfiguration getTestConfig(
+        String ip
+    ) throws Exception {
+        if (ip.equals("224.5.6.7:9000")) {
+            return new EndpointConfiguration(
+                "224.5.6.7:9000",
+                "Chat of Secret Oriental Culinary",
+                "AES",
+                256,
+                "GCM",
+                "PKCS5Padding",
+                "SHA256",
+                "HMacSHA256",
+                256
+            );
+        } else
+            throw new Exception("Configuration not found");
     }
 }
